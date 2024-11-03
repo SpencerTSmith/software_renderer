c7bbcd17e4440e104da8721cd43d053076e32f3b
SpencerTSmith <spencertsmith2003@gmail.com>
2024-11-03 01:48:46 -0500

    begin work on possible dynamic array refactor

--- src/array.h
+++ src/array.h
@@ -1,6 +1,8 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
+#include <stddef.h>
+
 // preprocessor function for fast push backs
 #define array_push(array, value)                                                                   \
     do {                                                                                           \
@@ -8,8 +10,24 @@
         (array)[array_size(array) - 1] = (value);                                                  \
     } while (0)
 
-void *array_hold(void *array, int count, int size);
+void *array_hold(void *array, int count, int element_size);
 int array_size(void *array);
 void array_free(void *array);
+
+// typedef enum {
+//     FACE_T,
+//     VERT_T,
+// } darray_type_e;
+
+typedef struct {
+    int capacity;
+    int occupied;
+    void *data;
+} darray_t;
+
+void darray_reserve(darray_t array, int count, size_t element_size);
+// not type safe
+void darray_pushback(darray_t array, const void *value);
+void darray_free(darray_t array);
 
 #endif
